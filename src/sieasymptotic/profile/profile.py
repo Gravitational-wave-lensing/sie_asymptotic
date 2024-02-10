9236e3b7b0134b4dff5319cca064da8b400a360e
Otto Akseli Hannuksela <otto.akseli.hannuksela@gmail.com>
2024-02-10 22:35:09 +0800

    restructuring

---
+++ src/sieasymptotic/profile/profile.py
@@ -0,0 +1,2 @@
+# Deflection potential, dimensionless fermat potential, magnification, and time delay for the SIE lens
+
--- src/sieasymptotic/solver/solve_image_positions.py
+++ src/sieasymptotic/solver/solve_image_positions.py
@@ -2,36 +2,7 @@
 from jax import numpy as jnp
 import jax
 import numpy as np
-
-def transform_cartesian_to_polar(x, y, omegatilde=0):
-    """Transform Cartesian coordinates to polar coordinates.
-
-    Args:
-        x (jnp.array): The x-coordinate of the position.
-        y (jnp.array): The y-coordinate of the position.
-        omegatilde (int, optional): The angle between the major axis of the lens and the x-axis. Defaults to 0.
-
-    Returns:
-        jnp.array: The radial and angular coordinates of the position.
-    """
-    r = jnp.sqrt(x**2 + y**2)
-    phi = jnp.arctan2(y, x)-omegatilde
-    return jnp.array([r, phi])
-
-def transform_polar_to_cartesian(r, phi, omegatilde=0):
-    ''' Transform polar coordinates to Cartesian coordinates.
-    
-    Args:
-        r (jnp.array): The radial coordinate of the position.
-        phi (jnp.array): The angular coordinate of the position.
-        omegatilde (float, optional): The angle between the major axis of the lens and the x-axis.
-    
-    Returns:
-        jnp.array: The x and y coordinates of the position.
-    '''
-    x = r*jnp.cos(phi+omegatilde)
-    y = r*jnp.sin(phi+omegatilde)
-    return jnp.array([x, y])
+from sieasymptotic.geom.geometry import transform_cartesian_to_polar, transform_polar_to_cartesian
 
 def solve_image_positions_polar(source_r, source_theta, f, omegatilde=0):
     ''' Solve the lens equation for the SIE lens, using the asymptotic expansion, in polar coordinates.
