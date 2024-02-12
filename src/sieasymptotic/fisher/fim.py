3d66a219db52f44b6a70b78f4c06fee9ea99f679
Otto Akseli Hannuksela <otto.akseli.hannuksela@gmail.com>
2024-02-12 10:29:38 +0800

    Fixed constant factor

--- src/sieasymptotic/fisher/fim.py
+++ src/sieasymptotic/fisher/fim.py
@@ -29,4 +29,28 @@ def chi_squared(log_T_star, log_dL, f, source_r, source_phi, log_dL_effectives_m
     chi_squared = 0
     chi_squared += jnp.sum((log_dL_effectives - log_dL_effectives_median)**2/log_sigma_dL_effectives**2)
     chi_squared += jnp.sum((log_time_delays - log_time_delays_median)**2/log_sigma_time_delays**2)
-    return None
+    return chi_squared
+
+def fisher_information_matrix( log_T_star, log_dL, f, source_r, source_phi, log_dL_effectives_median, log_time_delays_median, log_sigma_dL_effectives=jnp.ones(4)*0.1, log_sigma_time_delays=jnp.ones(3)*0.03, omegatilde=0):
+    """Computes the fisher information matrix for the SIE lens moel with asymptotic expansion using jax
+
+    Args:
+        T_star (jnp.array): The time-delay factor
+        dL (jnp.array): The luminosity distance.
+        f (jnp.array): The axis ratio
+        source_r (jnp.array): The radial coordinate of the source position.
+        source_phi (jnp.array): The polar coordinate of the source position.
+        log_dL_effectives_median (jnp.array): The median logarithm of the effective luminosity distances.
+        log_time_delays_median (jnp.array): The median logarithm of the time delays.
+        log_sigma_dL_effectives (jnp.array, optional): The logarithm of the effective luminosity distance uncertainties. Defaults to jnp.ones(4)*0.1.
+        log_sigma_time_delays (jnp.array, optional): The logarithm of the time delay uncertainties. Defaults to jnp.ones(3)*0.03.
+        omegatilde (float, optional): The angle between the source and the lens. Defaults to 0.
+
+    Returns:
+        jnp.array: The fisher information matrix.
+    """
+    # The fisher information matrix is the hessian of the chi_squared distribution with respect to T_star, dL, f, source_r, source_phi
+    chi_squared_grad = grad(chi_squared)
+    chi_squared_hessian = hessian(chi_squared, argnums=(0, 1, 2, 3, 4))
+    fisher_information = chi_squared_hessian(log_T_star, log_dL, f, source_r, source_phi, log_dL_effectives_median, log_time_delays_median, log_sigma_dL_effectives, log_sigma_time_delays, omegatilde)/2
+    return fisher_information
